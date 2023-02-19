5e0b1c7ba53a10d7c55e972e6e4ff4ef6bdc320b
LiKang6688 <kkllpaul7766@gmail.com>
2023-02-19 21:36:39 +0100

    feat: seo and analyze

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import ThemeContext, { Theme } from "@/lib/themeContext";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 
@@ -30,6 +31,21 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <DefaultSeo
+          title="Next SEO Example"
+          description="Next SEO is a plug in that makes managing your SEO easier in Next.js projects."
+          openGraph={{
+            type: "website",
+            locale: "en_IE",
+            url: "https://www.url.ie/",
+            siteName: "SiteName",
+          }}
+          twitter={{
+            handle: "@handle",
+            site: "@site",
+            cardType: "summary_large_image",
+          }}
+        />
         <div
           style={{
             width: "100%",
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,37 @@
 import Widget from "@/components/Widget";
+import { NextSeo } from "next-seo";
 import Image from "next/image";
 
 function About() {
   return (
     <>
+      <NextSeo
+        title="Manage SEO in NextJS with Next SEO"
+        description="Next SEO packages simplifies the SEO management in Next Apps with less configurations"
+        canonical="www.example.com/next-seo-blog"
+        openGraph={{
+          type: "article",
+          article: {
+            publishedTime: "2022-06-21T23:04:13Z",
+            modifiedTime: "2022-01-21T18:04:43Z",
+            authors: [
+              "https://www.example.com/authors/@firstnameA-lastnameA",
+              "https://www.example.com/authors/@firstnameB-lastnameB",
+            ],
+            tags: ["Tag A", "Tag B", "Tag C"],
+          },
+          url: "www.example.com/next-seo-blog",
+          images: [
+            {
+              url: "https://www.test.ie/images/cover.jpg",
+              width: 850,
+              height: 650,
+              alt: "Photo of text",
+            },
+          ],
+          site_name: "Next Blog",
+        }}
+      />
       <h1>About Page</h1>
       <Image
         src="https://images.unsplash.com/photo-1605460375648-278bcbd579a6"
